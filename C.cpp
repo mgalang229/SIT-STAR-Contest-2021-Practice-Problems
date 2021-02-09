3cee90129115411ec379bc327262b7ce58d1238e
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-02-09 23:37:47 +0800

    Update C.cpp

--- C.cpp
+++ C.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main() {                            
-  ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
  	int t0, t1, t2;
  	cin >> t0 >> t1 >> t2;
  	if (abs(t0 - t1) < abs(t0 - t2)) {
