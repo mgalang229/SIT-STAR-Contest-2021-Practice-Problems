77be1655c6eab0c6fe1b961ddc551465863ea6f0
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-02-09 23:36:29 +0800

    Create C.cpp

---
+++ C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {                            
+  ios::sync_with_stdio(false);
+ 	int t0, t1, t2;
+ 	cin >> t0 >> t1 >> t2;
+ 	if (abs(t0 - t1) < abs(t0 - t2)) {
+ 		cout << abs(t0 - t1) + abs(t1 - t2);
+ 	} else {
+ 		cout << abs(t0 - t2) + abs(t2 - t1);
+ 	}
+ 	cout << '\n';
+	return 0;
+}      
